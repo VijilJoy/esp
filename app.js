b345181fc64224d33d372e13b5264d40390076c9
unknown <vijiljoy79@gmail.com>
2024-10-25 19:58:53 +0530

    Added Docker File

---
+++ app.js
@@ -0,0 +1,29 @@
+// server.js
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const port = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3000;
+// const server = new WebSocket.Server({
+//   port: port,
+// });
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// server.on("connection", (ws) => {
+//   console.log("Client connected");
+
+//   ws.on("message", (message) => {
+//     console.log(`Received: ${message}`);
+//     // Echo the message back to the client
+//     ws.send(`Server received: ${message}`);
+//   });
+
+//   ws.on("close", () => {
+//     console.log("Client disconnected");
+//   });
+// });
+console.log(`Server listening on port ${port}`);
