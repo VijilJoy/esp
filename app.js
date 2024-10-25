75361373b2ef49416985fc644d0276c73e36a462
unknown <vijiljoy79@gmail.com>
2024-10-25 20:22:22 +0530

    changed

--- app.js
+++ app.js
@@ -4,26 +4,24 @@ const express = require("express");
 const app = express();
 const port = process.env.X_ZOHO_CATALYST_LISTEN_PORT || 3000;
 const server = new WebSocket.Server({
-  port: port,
+  port: 8081,
 });
-// app.get("/", (req, res) => {
-//   res.send("hello");
-// });
-// app.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+  server.on("connection", (ws) => {
+    console.log("Client connected");
 
-server.on("connection", (ws) => {
-  console.log("Client connected");
+    ws.on("message", (message) => {
+      console.log(`Received: ${message}`);
+      // Echo the message back to the client
+      ws.send(`Server received: ${message}`);
+    });
 
-  ws.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    // Echo the message back to the client
-    ws.send(`Server received: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
   });
 });
-console.log(`Server listening on port ${port}`);
