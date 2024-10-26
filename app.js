12de650678e77969478de7ce32a90fbcb9fcf297
unknown <vijiljoy79@gmail.com>
2024-10-26 23:20:14 +0530

    d

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 // server.js
+const { json } = require("express");
 const WebSocket = require("ws");
 const port = process.env.port || 3000;
 
 // Set up WebSocket server
 const wss = new WebSocket.Server({ port });
 
+const handleMsg = (req, res) => {
+  try {
+    val = JSON.parse(req);
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(val));
+      }
+    });
+    console.log(val);
+  } catch (e) {
+    console.log("error parsing json");
+  }
+};
+
 wss.on("connection", (ws) => {
   console.log("New Client");
-
   // Keep-alive ping every 30 seconds
   const keepAliveInterval = setInterval(() => {
     if (ws.readyState === WebSocket.OPEN) {
@@ -20,8 +34,7 @@ wss.on("connection", (ws) => {
   });
 
   ws.on("message", (message) => {
-    console.log(`Received: ${message}`);
-    ws.send(`Server received: ${message}`);
+    handleMsg(message, ws);
   });
 
   ws.on("close", () => {
