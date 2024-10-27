4f1519c9a1e321a080ce08021f6884cb645ccc59
unknown <vijiljoy79@gmail.com>
2024-10-27 21:59:18 +0530

    a

--- app.js
+++ app.js
@@ -1,115 +1,81 @@
-// const express = require("express");
-// const WebSocket = require("ws");
-// const http = require("http");
-// const cors = require("cors");
-// const port = process.env.PORT || 8080;
-
-// let lastData = {};
-
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-// const server = http.createServer(app);
-// const wss = new WebSocket.Server({ server });
-
-// app.post("/data", (req, res) => {
-//   const data = req.body;
-//   res.send(data);
-//   console.log(data);
-
-//   wss.clients.forEach((client) => {
-//     if (client.readyState === WebSocket.OPEN) {
-//       try {
-//         client.send(JSON.stringify(data));
-//       } catch (e) {
-//         console.error("Error sending message to client:", e);
-//       }
-//     }
-//   });
-// });
-
-// app.get("/data", (req, res) => {
-//   if (lastData) {
-//     res.setHeader("Content-Type", "application/json");
-//     res.send(JSON.stringify(lastData));
-//     res.status(200);
-//   } else {
-//     res.send("null");
-//     res.status(400);
-//   }
-// });
-
-// const handleMsg = (message) => {
-//   try {
-//     const val = JSON.parse(message);
-//     lastData = val;
-//     wss.clients.forEach((client) => {
-//       if (client.readyState === WebSocket.OPEN) {
-//         client.send(JSON.stringify(val));
-//       }
-//     });
-//     console.log(val);
-//   } catch (e) {
-//     console.error("Error parsing JSON:", e);
-//   }
-// };
-
-// wss.on("connection", (ws) => {
-//   console.log("New Client");
-
-//   // Keep-alive ping every 30 seconds
-//   const keepAliveInterval = setInterval(() => {
-//     if (ws.readyState === WebSocket.OPEN) {
-//       ws.ping();
-//     }
-//   }, 30000);
-
-//   ws.on("pong", () => {
-//     console.log("Pong received from client");
-//   });
-
-//   ws.on("message", (message) => {
-//     handleMsg(message);
-//   });
-
-//   ws.on("close", () => {
-//     clearInterval(keepAliveInterval);
-//     console.log("Client disconnected");
-//   });
-// });
-
-// server.listen(port, () => console.log(`Server running on port ${port}`));
-
+const express = require("express");
 const WebSocket = require("ws");
 const http = require("http");
+const cors = require("cors");
+const port = process.env.PORT || 8080;
 
-// Create an HTTP server
-const server = http.createServer();
+let lastData = {};
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Set the port from the environment variable or use a default value
-const PORT = process.env.PORT || 3000;
+app.post("/data", (req, res) => {
+  const data = req.body;
+  res.send(data);
+  console.log(data);
 
-wss.on("connection", (ws) => {
-  console.log("New client connected.");
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      try {
+        client.send(JSON.stringify(data));
+      } catch (e) {
+        console.error("Error sending message to client:", e);
+      }
+    }
+  });
+});
 
-  ws.on("message", (message) => {
-    console.log(`Received message: ${message}`);
+app.get("/data", (req, res) => {
+  if (lastData) {
+    res.setHeader("Content-Type", "application/json");
+    res.send(JSON.stringify(lastData));
+    res.status(200);
+  } else {
+    res.send("null");
+    res.status(400);
+  }
+});
 
-    // Broadcast the message to all connected clients
+const handleMsg = (message) => {
+  try {
+    const val = JSON.parse(message);
+    lastData = val;
     wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
+        client.send(JSON.stringify(val));
       }
     });
+    console.log(val);
+  } catch (e) {
+    console.error("Error parsing JSON:", e);
+  }
+};
+
+wss.on("connection", (ws) => {
+  console.log("New Client");
+
+  // Keep-alive ping every 30 seconds
+  const keepAliveInterval = setInterval(() => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.ping();
+    }
+  }, 30000);
+
+  ws.on("pong", () => {
+    console.log("Pong received from client");
+  });
+
+  ws.on("message", (message) => {
+    handleMsg(message);
   });
 
   ws.on("close", () => {
-    console.log("Client disconnected.");
+    clearInterval(keepAliveInterval);
+    console.log("Client disconnected");
   });
 });
 
-// Start the server
-server.listen(PORT, () => {
-  console.log(`WebSocket server is running on port ${PORT}...`);
-});
+server.listen(port, () => console.log(`Server running on port ${port}`));
