22b5e8fe344aa3cbbb6daa15b59ecd29e6bcd7b1
unknown <vijiljoy79@gmail.com>
2024-10-27 16:52:44 +0530

    a

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
